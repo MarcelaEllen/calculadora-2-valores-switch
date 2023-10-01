5d28089cf9adbfd26b26f6d324d8095ad839821e
Marcela Ellen <111468086+MarcelaEllen@users.noreply.github.com>
2023-10-01 16:15:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+
 //calculadora 2 valores
 let num1 = parseFloat(prompt("Digite um número"))
 let num2 = parseFloat(prompt("Digite outro número"))
-let op = prompt("Agora escolha uma operação, digite o número correspondente a ela: 1- soma, 2- subtração, 3- multiplicação e 4- divisão")
+let op = prompt("Agora escolha uma operação, digite: + para soma, - para subtração, * para multiplicação e / para divisão")
 
 let resultado
 
@@ -9,26 +10,29 @@ num1 = parseFloat(num1);
 num2 = parseFloat(num2);
 
 switch (op) {
-    case '1':
+    case '+':
         resultado = num1 + num2;
         break;
-      case '2':
+    case '-':
         resultado = num1 - num2;
         break;
-      case '3':
+    case '*':
         resultado = num1 * num2;
         break;
-        case '4':
-            if (num2 === 0) {
-                console.log("Divisão por zero não é permitida.");
-            } else {
-                resultado = num1 / num2;
-                let sobra = num1 % num2;
-                console.log("O resto da divisão é: " + sobra);
-            }
-            break;
+    case '/':
+        if (num2 === 0) {
+            console.log("Divisão por zero não é permitida.");
+        } else {
+            resultado = num1 / num2;
+            let sobra = num1 % num2;
+            console.log("O resto da divisão é: " + sobra);
+        }
+        break;
     default:
-        console.log("Digite uma opção válida. 1, 2, 3 ou 4!")
+        console.log("Operador inválido. Use +, -, *, ou /")
         break;
 }
-    console.log("Resultado: " + resultado)
+
+if (resultado !== undefined && !isNaN(resultado)) {
+    console.log("Resultado: " + resultado);
+}
